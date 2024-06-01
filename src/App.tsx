f7175536ebc984a189d8283896cda0b50f7a2dc9
matthew-nascimento_valtech <matthew.nascimento@valtech.com>
2024-06-01 10:43:04 -0300

    feat: Semantically improved HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import './App.scss'
-import omelette from './assets/images/image-omelette.jpeg'
+import './App.scss';
+import omelette from './assets/images/image-omelette.jpeg';
 
 function App() {
-
   return (
     <main className='recipe'>
-      <div className="recipe__card">
-        <img className="recipe__image" src={omelette} alt="omelette" />
+      <article className="recipe__card">
+        <figure className="recipe__imageWrapper">
+          <img className="recipe__image" src={omelette} alt="omelette" />
+        </figure>
         <div className="recipe__body">
-          <h1 className="recipe__title">Simple Omelette Recipe</h1>
-          <p className="recipe__description">An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.</p>
-          <div className="recipe__prepareTime">
+          <header className="recipe__header">
+            <h1 className="recipe__title">Simple Omelette Recipe</h1>
+            <p className="recipe__description">An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.</p>
+          </header>
+          <section className="recipe__prepareTime">
             <h2 className="recipe__listTitle">Preparation time</h2>
             <ul className="recipe__list">
               <li className="recipe__item">
@@ -18,14 +21,13 @@ function App() {
               </li>
               <li className="recipe__item">
                 <p><span>Preparation: </span> 5 minutes</p>
-
               </li>
               <li className="recipe__item">
                 <p><span>Cooking: </span> 5 minutes</p>
               </li>
             </ul>
-          </div>
-          <div className="recipe__ingredients">
+          </section>
+          <section className="recipe__ingredients">
             <h2 className="recipe__subtitle">Ingredients</h2>
             <ul className="recipe__list">
               <li className="recipe__item">
@@ -33,7 +35,6 @@ function App() {
               </li>
               <li className="recipe__item">
                 <p>Salt, to taste</p>
-
               </li>
               <li className="recipe__item">
                 <p>Pepper, to taste</p>
@@ -45,16 +46,15 @@ function App() {
                 <p>Optional fillings: cheese, diced vegetables, cooked meats, herbs</p>
               </li>
             </ul>
-          </div>
-          <div className="recipe__instructions">
+          </section>
+          <section className="recipe__instructions">
             <h2 className="recipe__subtitle">Instructions</h2>
             <ol className="recipe__listNumber">
               <li className="recipe__itemNumber">
-                <p><span>Beat the eggs: </span>  In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.</p>
+                <p><span>Beat the eggs: </span> In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.</p>
               </li>
               <li className="recipe__itemNumber">
                 <p><span>Heat the pan: </span> Place a non-stick frying pan over medium heat and add butter or oil.</p>
-
               </li>
               <li className="recipe__itemNumber">
                 <p><span>Cook the omelette: </span> Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.</p>
@@ -66,16 +66,15 @@ function App() {
                 <p><span>Fold and serve: </span> As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.</p>
               </li>
               <li className="recipe__itemNumber">
-                <p><span>Enjoy: </span> Serve hot, with additional salt and pepper if needed..</p>
+                <p><span>Enjoy: </span> Serve hot, with additional salt and pepper if needed.</p>
               </li>
             </ol>
-          </div>
-          <div className="recipe__nutrition">
+          </section>
+          <section className="recipe__nutrition">
             <h2 className="recipe__subtitle">Nutrition</h2>
             <p className="recipe__description">
               The table below shows nutritional values per serving without the additional fillings.
             </p>
-
             <div>
               <div className="recipe__nutrient">
                 <p className="recipe__nutrientTitle">Calories</p>
@@ -94,12 +93,11 @@ function App() {
                 <p className="recipe__nutrientValue">22g</p>
               </div>
             </div>
-
-          </div>
+          </section>
         </div>
-      </div>
+      </article>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
